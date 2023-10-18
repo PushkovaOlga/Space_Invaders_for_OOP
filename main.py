97ba3dd1ac7fdfa18dbc940c57305ea42d3668d5
Blowsight71345 <idiot71345@gmail.com>
2023-10-18 19:07:04 +0300

    add images and obj maincharacter

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import sys
-
+from main_character import MainCharacter
 
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
     pygame.display.set_caption("SpaxeX by Pushkova")
+    maincharacter = MainCharacter(screen)
 
     flag = True
     while flag:
@@ -13,6 +14,7 @@ def start_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        maincharacter.output()
         pygame.display.flip()
 
 
--- main_character.py
+++ main_character.py
@@ -1,5 +1,13 @@
 import pygame
 
 class MainCharacter():
-    def __init__(self, image):
+    def __init__(self, screen):
+        self.screen = screen
         self.image = pygame.image.load("images/pixil-frame-0 (1).png")
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def output(self):
+        self.screen.blit(self.image, self.rect)
