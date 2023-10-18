0b7bf38f4976a55e366c444cf35cd89453e9a97c
Blowsight71345 <idiot71345@gmail.com>
2023-10-18 11:05:48 +0300

    add images

--- main_character.py
+++ main_character.py
@@ -2,4 +2,4 @@ import pygame
 
 class MainCharacter():
     def __init__(self, image):
-        self.image = pygame.image.load()
+        self.image = pygame.image.load("images/pixil-frame-0 (1).png")
