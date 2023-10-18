e682187dfca6531c57ed28a36f3ca683ac1d9f83
TatianaMaksakova <tatzianar@gmail.com>
2023-10-18 16:34:15 +0300

    add: add class(Character update: update start_game function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-
+from Hero import MainCharacter
 
 def start_game ():
     pygame.init()
@@ -8,11 +8,13 @@ def start_game ():
     pygame.display.set_caption("SpaceX by Andrei")
 
     flag = True
+    hero = MainCharacter(screen)
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
             pygame.display.flip()
+            hero.output()
 
 start_game()
