49c85c690e688e7202bce813bd07efc99338a4eb
TatianaMaksakova <tatzianar@gmail.com>
2023-10-18 16:34:08 +0300

    add: directory images

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import pygame
 import sys
-
+from main_character import MainCharacter
 
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
-    pygame.display.set_caption("SpaxeX by Pushkova")
+    pygame.display.set_caption("SpaxeX by Semakov")
 
     flag = True
+    maincharacter = MainCharacter(screen)
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
         pygame.display.flip()
+        maincharacter.output()
 
 
 start_game()
