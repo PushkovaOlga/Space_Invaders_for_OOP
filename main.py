7cc96ce19d42fd4793f257eb8282e26c14c40473
MDMAenjoyer <vikaonelove4me@gmail.com>
2023-10-19 16:05:22 +0300

    add:class bullet add: class enemies add: movement add: background, bullet image and hero image

--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 import pygame
 import sys
+from pygame.locals import *
+from Character import *
 
+pygame.init()
 
-def start_game():
-    pygame.init()
-    screen = pygame.display.set_mode((1000, 800))
-    pygame.display.set_caption("SpaxeX by Pushkova")
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Space Invaders')
 
-    flag = True
-    while flag:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+background_img = pygame.image.load('background.jpg')
 
-        pygame.display.flip()
+player = Player()
 
+enemies = []
+for i in range(6):
+    enemy = Enemy()
+    enemies.append(enemy)
 
-start_game()
+bullets = []
+
+while True:
+    screen.blit(background_img, (0, 0))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+        if event.type == KEYDOWN:
+            player.handle_movement(event)
+
+            if event.key == pygame.K_SPACE:
+                bullet = Bullet(player.x, player.y)
+                bullets.append(bullet)
+
+    player.check_boundary()
+
+    player.draw(screen)
+
+    for enemy in enemies:
+        enemy.move()
+        enemy.draw(screen)
+
+    for bullet in bullets:
+        bullet.move()
+        bullet.draw(screen)
+
+        if bullet.y < 0:
+            bullets.remove(bullet)
+
+    pygame.display.update()
