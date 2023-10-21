e8049caeacc21cf657dda7c4cef8bb15b0130e67
Blowsight71345 <idiot71345@gmail.com>
2023-10-21 14:25:31 +0300

    update moving main character

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from main_character import MainCharacter
 
+
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000, 800))
@@ -10,12 +11,26 @@ def start_game():
 
     flag = True
     while flag:
+        '''обработка событий игрока'''
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    maincharacter.move_right = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    maincharacter.move_right = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    maincharacter.move_left = True
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    maincharacter.move_left = False
 
         maincharacter.output()
         pygame.display.flip()
+        maincharacter.moving(screen)
 
 
 start_game()
--- main_character.py
+++ main_character.py
@@ -1,13 +1,27 @@
 import pygame
 
+
 class MainCharacter():
     def __init__(self, screen):
+        '''основные характеристики'''
         self.screen = screen
-        self.image = pygame.image.load("images/pixil-frame-0 (1).png")
+        self.image = pygame.image.load("images/pixil-frame-0.png")
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        self.move_right = False
+        self.move_left = False
 
     def output(self):
+        '''отрисовка героя на экране'''
         self.screen.blit(self.image, self.rect)
+
+    def moving(self, screen):
+        '''движение героя по оси Х'''
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        if self.move_left and self.rect.left > 0:
+            self.rect.centerx -= 1
+        '''обновление экрана после движения для избавления от растягивая изображения'''
+        screen.fill(0)
