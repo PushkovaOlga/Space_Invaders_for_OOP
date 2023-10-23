a9c4f208606f16c014909a855ac0279b493d4eeb
danilada <dp9993778@gmail.com>
2023-10-23 19:46:09 +0300

    add: poliakov 23/2 RPO

--- Superbustykiller.py
+++ Superbustykiller.py
@@ -1,5 +1,21 @@
 import pygame
 
 class Superhero():
-    def __init__(self):
-        self.image = pygame.image.load('D:\Space_livegame\images')
+    def init(self, screen):
+        self.image = pygame.image.load("Images/hero.png")
+        self.screen = screen
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+        self.move_right = False
+        self.move_left = False
+
+
+    def output(self):
+        self.screen.blit(self.image, self.rect)
+    def moving(self):
+        if self.move_right and self.rect.right < self.screen_rect.right:
+            self.rect.centerx += 1
+        elif self.move_left and self.rect.left > 0:
+            self.rect.centerx -= 1
--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pygame
 import sys
+from Superbustykiller import Superhero
+
 def start_game():
     pygame.init()
     screen = pygame.display.set_mode((1000,800))
-    pygame.display.set_caption("SpeceX by poliakov")
+    pygame.display.set_caption("SpaceX by Karachevtsev")
+    maincharacter = Superhero()
 
     flag = True
     while flag:
-       for event in pygame.event.get():
-           if event.type == pygame.QUIT:
-               sys.exit()
-       pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.rect.centerx += 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    maincharacter.move_right = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    maincharacter.rect.centerx -= 1
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    maincharacter.move_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    maincharacter.move_left = False
 
-
+        maincharacter.output()
+        pygame.display.flip()
 start_game()
